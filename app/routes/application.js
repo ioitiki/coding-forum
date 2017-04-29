505d734cd5e0b63a6faf1c946fba8e1c43e395ad
Andrew Malkin <andymalkin@hotmail.com>
2017-04-29 13:41:18 -0700

    add nav-bar and footer components along with building out several templates

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,30 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    return this.get('session').fetch().catch(function() {
-    });
-  },
-
-  actions: {
-    signIn: function(provider) {
-      this.get('session').open('firebase', {
-        provider: provider,
-      }).then(data => {
-        var userID = data.currentUser.uid;
-        this.get('getUser').getUser(userID).catch(error => {
-          var params = {
-            name: data.currentUser.displayName,
-            userID: userID,
-            photoURL: data.currentUser.photoURL
-          };
-          var newUser = this.get('store').createRecord('user', params);
-          newUser.save();
-        });
-      });
-    },
-
-    signOut: function() {
-      this.get('session').close();
-    },
+    return this.get('session').fetch().catch(function() {});
   },
 });
