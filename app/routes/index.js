e7c06ea038d9436f3ae9994131aba8efe58ef8ce
Andrew Malkin <andymalkin@hotmail.com>
2017-04-30 14:39:07 -0700

    add question-list compent for displaying a a list of all questions on the homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,34 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+var questions = [{
+  id: 1,
+  author: "Veruca Salt",
+  subject: "JavaScript question",
+  body: "How do you properly implement promises when dealing with async functinos?",
+}, {
+  id: 2,
+  author: "Bob Jones",
+  subject: "CSS problem",
+  body: "How do I make nested list?",
+}];
+
+export default Ember.Route.extend( {
+  model() {
+      return this.store.findAll('question');
+    },
+
   session: Ember.inject.service('session'),
 
-  beforeModel: function() {
+  beforeModel() {
     if (!this.get('session.isAuthenticated')) {
       this.transitionTo('index');
     }
   },
+
+  actions: {
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
